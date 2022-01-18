41032565f2385ceb626319f9076e929828740c9e
Matthias Larsen <matthias@msalconsulting.dk>
2022-01-18 10:42:14 +0100

    feat(node): model & resources

---
+++ database/factories/NodeFactory.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Node;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class NodeFactory extends Factory
+{
+    public function definition()
+    {
+        return [
+            'name' => $this->faker->name(),
+//            'parent_id' => Node::factory(),
+            'is_manager' => $this->faker->boolean(),
+            'height' => fn ($attr) => ($attr['parent_id']?->height ?? 0) + 1,
+            'metadata' => fn ($attr) => $attr['is_manager']
+                ? ['department' => $this->faker->company]
+                : ['language' => $this->faker->languageCode],
+        ];
+    }
+
+    public function root()
+    {
+        return $this->state(function (array $attributes) {
+            return [
+                'parent_id' => null,
+            ];
+        });
+    }
+}
---
+++ database/migrations/2022_01_18_090803_create_nodes_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateNodesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('nodes', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('parent_id')->nullable();
+            $table->string('name');
+            $table->tinyInteger('height')->default(0);
+            $table->boolean('is_manager')->default(0);
+            $table->string('metadata');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('nodes');
+    }
+}
